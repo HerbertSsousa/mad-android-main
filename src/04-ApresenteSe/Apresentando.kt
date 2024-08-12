5a91410f853285db2a2c565ccce7c7c7cb55dff7
HerbertSsousa <herbert.ssousa@gmail.com>
2024-08-12 20:25:33 -0300

    atualizando conteudo apresenteSe

---
+++ src/04-ApresenteSe/Apresentando.kt
@@ -0,0 +1,15 @@
+// comentario linha
+
+
+
+fun main() {
+
+    //Declaração de variável
+    var idade = 19      //Idade do professor
+    var altura = 1.75f  //Altura do professor (usando o 'f' para indicar que é um float)
+    var inicial  = 'H'  // Inicial do nome do professor 
+
+    // Impressão das informações do indivíduo usando interploação de strings 
+    println("Minha inicial e $inicial, tenho $idade anos e minha $altura e %.2f".format(altura))
+
+}
