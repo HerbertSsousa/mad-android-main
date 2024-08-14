c237f8f908f06fe46e42a085b6b64cf1259e945d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 20:05:03 -0300

    adicionando mais arquivos de ex

---
+++ exercicioMedia/DesafioNotasInput.kt
@@ -0,0 +1,21 @@
+fun main(){
+    println("Digite a nota da primeira avaliacao: ")
+    val nota1 = readLine()!!.toDouble()
+
+    println("Digite a nota da segunda avaliacao: ")
+    val nota2 = readLine()!!.toDouble()
+
+    println("Digite a nota da terceira avaliacao: ")
+    val nota3 = readLine()!!.toDouble()
+
+
+    val media = (nota1 + nota2 + nota3) / 3
+    val mediaFormatada = String.format("%.2f", media)
+    //println("A média e: $media")
+
+    //formatando media escolar 
+    println("A media e: $media ou mediaformatada $mediaFormatada")
+
+}
+
+// para rodar no terminal escreva "java -jar DesafioNotasinput.jar"
